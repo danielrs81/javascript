cd37b3487d62a781a34ee09779fb82507f445452
Daniel Rodrigues da Silva <115389640+danielrs81@users.noreply.github.com>
2022-11-10 14:51:57 -0300

    update

--- CursoJS/aula13/ambiente.js
+++ CursoJS/aula13/ambiente.js
@@ -1,8 +1,17 @@
 var c = 1
-while (c < 6) {
-    console.log('Olá, tudo bem!')
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) 
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
     c++
 }
+*/
+
 
 
 
@@ -14,4 +23,5 @@ console.log('Olá, tudo bem!')
 console.log('Olá, tudo bem!')
 console.log('Olá, tudo bem!')
 */
+
 
---
+++ CursoJS/aula14/ambiente.js
@@ -0,0 +1,6 @@
+console.log('Vai começar...') 
+for(var c = 1;c <= 10;c++) {
+    console.log(`8 x ${c} = `)    
+    console.log(8 * c)
+}
+console.log('Fim!')

5fae41a6f1da8aa0ee2218f90dd26b5eff9c55cb
Daniel Rodrigues da Silva <115389640+danielrs81@users.noreply.github.com>
2022-11-10 12:04:21 -0300

    up

--- CursoJS/aula12/ex015/script/script.js
+++ CursoJS/aula12/ex015/script/script.js
@@ -3,7 +3,7 @@ function verificar() {
     var ano = data.getFullYear()
     var formularioano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (formularioano.value.length == 0 || Number(formularioano.value) > ano) {
+    if (formularioano.value.length == 0 || formularioano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var formulariosex = document.getElementsByName('radsex')
@@ -15,32 +15,42 @@ function verificar() {
             genero = 'Homem'
             if (idade >=0 && idade < 4){
                 // bebe
-                img.setAttribute('src', './imagens/bebe_menino.jpg')
+                img.setAttribute("src", "./imagens/bebe_h.png")
             } else if (idade < 10) {
                 //crianca
+                img.setAttribute("src", "./imagens/crianca_h.png")
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute("src", "./imagens/jovem_h.png")
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute("src", "./imagens/adulto_h.png")
             } else {
                 // Idoso
+                img.setAttribute("src", "./imagens/idoso_h.png")
             }
         
         } else if (formulariosex[1].checked) {
             genero = 'Mulher'
             if (idade >=0 && idade < 4){
                 // bebe
+                img.setAttribute("src", "./imagens/bebe_m.png")
             } else if (idade < 10) {
                 //crianca
+                img.setAttribute("src", "./imagens/crianca_m.png")
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute("src", "./imagens/jovem_m.png")
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute("src", "./imagens/adulto_m.png")
             } else {
                 // Idoso
+                img.setAttribute("src", "./imagens/idoso_m.png")
             }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.<br>`
+        res.appendChild(img)
     }
 }

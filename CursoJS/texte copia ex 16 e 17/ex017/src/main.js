efbdd8c3494cff02add4ff3e1115d02859ff9a82
Daniel Rodrigues da Silva <115389640+danielrs81@users.noreply.github.com>
2022-11-10 23:28:19 -0300

    up

--- CursoJS/texte copia ex 16 e 17/ex017/src/main.js
+++ CursoJS/texte copia ex 16 e 17/ex017/src/main.js
@@ -6,30 +6,7 @@
     );
   })();
 
-function tabuada() {
-    let num = window.document.getElementById('txtn')
-    let tab = window.document.getElementById('seltab')
-
-    if (num.value.length == 0) {
-        window.alert('Por favor, digite um número!')
-    } else {
-        let n = Number(num.value)
-        let c = 1
-        tab.innerHTML = ''
-        while ( c <= 10) {
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n * c}`
-            item.value = `tab${c}`
-            tab.appendChild(item)
-            c++
-        }
-    }
-}
-
-
-/*
-
-function tabuada() {
+function multiplicar() {
     let num = window.document.getElementById('txtn')
     let tab = window.document.getElementById('seltab')
 
@@ -46,5 +23,3 @@ function tabuada() {
         }
     }
 }
-
-*/

2b4dc2aaa02448d0d54cec6abf4c3916d5bc1db8
Daniel Rodrigues da Silva <115389640+danielrs81@users.noreply.github.com>
2022-11-11 23:26:36 -0300

    up

---
+++ CursoJS/aula15/ambiente.js
@@ -0,0 +1,14 @@
+let num =[3,2,0]
+num[3] = 6
+num.length
+num.sort()
+num.push(1)
+
+console.log(num)
+console.log(`O vetor tem esses ${num} elementos`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro vetor é ${num[0]}`)
+
+for(let pos=0; pos<num.length; pos++){
+    console.log(bum[pos])
+}

5f6991cd16c429efaac0949abaf035b01c8d922a
Daniel Rodrigues da Silva <115389640+danielrs81@users.noreply.github.com>
2022-11-16 18:06:09 -0300

    up

--- CursoJS/aula16ex/ex018/script/script.js
+++ CursoJS/aula16ex/ex018/script/script.js
@@ -1,1 +1,32 @@
-alert('Olá')
+let num = window.document.getElementById('fnum')
+let lista = window.document.getElementById('flista')
+let res = window.document.getElementById('res')
+let valores = []
+
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function inLista(n, 1) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('Tudo Ok')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+
+}
